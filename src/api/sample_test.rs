341df29c9ef693a53aea4107e21a838be7df9a60
rein <reinlein.k.smile14@gmail.com>
2026-09-04 00:38:55 +0900

    sample test

--- src/api/sample_test.rs
+++ src/api/sample_test.rs
@@ -2,12 +2,48 @@ use std::{
     io::{Write as _, stderr},
     path::Path,
     process::{self, Stdio},
+    thread,
+    time::{Duration, Instant},
 };
 
 use anyhow::{Context as _, Result};
+use colored::Colorize;
 use lazy_regex::lazy_regex;
 
 use crate::api::config::{Contest, Task};
+
+#[derive(Debug, PartialEq, Eq)]
+enum TestResult {
+    Ac,
+    Wa,
+    Re,
+    Tle,
+}
+
+fn is_correct(out: &str, correct: &str) -> bool {
+    let decimal_pattern = lazy_regex!(r#"^\d+\.\d+$"#);
+
+    if decimal_pattern.is_match(out) {
+        out.len() >= correct.len() && out[..correct.len() - 1] == correct[..correct.len() - 1]
+    } else {
+        out == correct
+    }
+}
+
+fn is_correct_all(out: &[u8], correct: &str) -> bool {
+    let out_divided: Vec<_> = String::from_utf8_lossy(out)
+        .into_owned()
+        .split_ascii_whitespace()
+        .map(str::to_string)
+        .collect();
+    let correct_divided: Vec<_> = correct.split_ascii_whitespace().collect();
+
+    out_divided.len() == correct_divided.len()
+        && out_divided
+            .iter()
+            .zip(&correct_divided)
+            .all(|(out, correct)| is_correct(out, correct))
+}
 
 pub fn sample_test(
     contest_dir: &Path,
@@ -34,52 +70,51 @@ pub fn sample_test(
         .write_all(sample_in.as_ref())
         .context("failed to run")?;
 
-    let output = child.wait_with_output().context("failed to run")?;
-
-    let out_divided: Vec<_> = String::from_utf8_lossy(&output.stdout)
-        .into_owned()
-        .split_ascii_whitespace()
-        .map(str::to_string)
-        .collect();
-    let correct_divided: Vec<_> = sample_out.split_ascii_whitespace().collect();
-
-    if output.status.success() {
-        if out_divided.len() == correct_divided.len()
-            && out_divided
-                .iter()
-                .zip(&correct_divided)
-                .all(|(out, correct)| is_correct(out, correct))
-        {
-            eprintln!("Sample{sample_number} ... AC!");
-            return Ok(true);
-        } else {
-            eprintln!("Sample{sample_number} ... WA!");
-            eprintln!("Standard input:");
-            eprintln!("{sample_in}");
-            eprintln!("Standard output:");
-            stderr().write_all(&output.stdout)?;
-            eprintln!("Expected output:");
-            eprintln!("{sample_out}");
+    let start_time = Instant::now();
+    let timeout = Duration::from_secs(6);
+    let is_tle = loop {
+        match child.try_wait()? {
+            Some(_) => break false,
+            None if start_time.elapsed() >= timeout => {
+                child.kill()?;
+                break true;
+            }
+            None => thread::sleep(Duration::from_millis(50)),
         }
+    };
+    let output = child.wait_with_output()?;
+
+    let result = if is_tle {
+        TestResult::Tle
+    } else if !output.status.success() {
+        TestResult::Re
+    } else if is_correct_all(&output.stdout, sample_out) {
+        TestResult::Ac
     } else {
-        eprintln!("Sample{sample_number} ... RE!");
-        if !output.stdout.is_empty() {
-            eprintln!("Standard output");
-            stderr().write_all(&output.stdout)?;
-        }
-        eprintln!("Standard error");
-        stderr().write_all(&output.stderr)?;
-    }
+        TestResult::Wa
+    };
 
-    Ok(false)
-}
+    let result_text = match result {
+        TestResult::Ac => "AC".on_green(),
+        TestResult::Wa => "wA".on_yellow(),
+        TestResult::Re => "RE".on_yellow(),
+        TestResult::Tle => "TLE".on_yellow(),
+    };
 
-fn is_correct(out: &str, correct: &str) -> bool {
-    let decimal_pattern = lazy_regex!(r#"^\d+\.\d+$"#);
+    eprintln!("-----------------------------------------");
+    eprintln!("Sample{sample_number} ... {result_text}");
+    eprintln!("Standard input:");
+    eprintln!("{sample_in}");
+    eprintln!("---------------");
+    eprintln!("Standard output:");
+    stderr().write_all(&output.stdout)?;
+    eprintln!("---------------");
+    eprintln!("Expected output:");
+    eprintln!("{sample_out}");
+    eprintln!("---------------");
+    eprintln!("Standard error:");
+    stderr().write_all(&output.stderr)?;
+    eprintln!("-----------------------------------------");
 
-    if decimal_pattern.is_match(out) {
-        out.len() >= correct.len() && out[..correct.len() - 1] == correct[..correct.len() - 1]
-    } else {
-        out == correct
-    }
+    Ok(result == TestResult::Ac)
 }
